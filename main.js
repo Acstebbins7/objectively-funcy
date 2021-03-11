32bacdfd357c5794bbefabe9ca6c3f350148be56
Adam Stebbins <acstebbins7@gmail.com>
2021-03-11 14:52:12 -0500

    Funcy

--- main.js
+++ main.js
@@ -1,8 +1,44 @@
-// Your code here.
+function getFirstName(persons){
+  return persons.firstName
+}
 
+function getLastName(persons){
+  return persons.lastName
+}
 
+function getFullName(persons){
+  return  getFirstName(persons) + ' ' + getLastName(persons)
+}
 
+function setFirstName(persons, name){
+  persons.firstName = name
+}
 
+function setAge (persons, age){
+  persons.age = age
+}
+
+function giveBirthday(persons){
+  if (persons.age !== undefined){
+    persons.age++
+  } else {
+    persons.age = 1
+  }
+}
+
+function marry(personA, personB){
+  personA.married = true
+  personB.married = true
+  personA.spouseName = getFullName(personB)
+  personB.spouseName = getFullName(personA)
+}
+
+function divorce(personA, personB){
+  personA.married = false
+  personB.married = false
+  delete personA.spouseName
+  delete personB.spouseName
+}
 
 // Our code here. Don't touch!
 if (typeof getFirstName === 'undefined') {
@@ -35,6 +71,18 @@ if (typeof marry === 'undefined') {
 
 if (typeof divorce === 'undefined') {
   divorce = undefined
+}
+
+
+module.exports = {
+  getFirstName,
+  getLastName,
+  getFullName,
+  setFirstName,
+  setAge,
+  giveBirthday,
+  marry,
+  divorce,
 }
 
 
