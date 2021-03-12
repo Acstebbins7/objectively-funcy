28854a0ba73e88b7528dfa79a707bf51e7227133
Adam Stebbins <acstebbins7@gmail.com>
2021-03-11 23:21:29 -0500

    Funcy Sauce.

--- main.js
+++ main.js
@@ -2,21 +2,31 @@ function getFirstName(persons){
   return persons.firstName
 }
 
+// $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+
 function getLastName(persons){
   return persons.lastName
 }
+
+// $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 
 function getFullName(persons){
   return  getFirstName(persons) + ' ' + getLastName(persons)
 }
 
+// $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+
 function setFirstName(persons, name){
   persons.firstName = name
 }
 
+// $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+
 function setAge (persons, age){
   persons.age = age
 }
+
+// $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 
 function giveBirthday(persons){
   if (persons.age !== undefined){
@@ -26,6 +36,8 @@ function giveBirthday(persons){
   }
 }
 
+// $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+
 function marry(personA, personB){
   personA.married = true
   personB.married = true
@@ -33,12 +45,16 @@ function marry(personA, personB){
   personB.spouseName = getFullName(personA)
 }
 
+// $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+
 function divorce(personA, personB){
   personA.married = false
   personB.married = false
   delete personA.spouseName
   delete personB.spouseName
 }
+
+// $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 
 // Our code here. Don't touch!
 if (typeof getFirstName === 'undefined') {
